22d437086227f3153c0dcd966708bd70efd4e5e1
TylerDevers <devers.tyler@gmail.com>
2017-11-27 11:55:56 -0800

    updated Oscillator.cs, obstacle now moves on its own

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour {
-	[SerializeField] Vector3 movementVector;
-	[Range(0,1)] [SerializeField] float movementFactor; // 1 is full move, 0 is no move
-	Vector3 startingPos;
+	[SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
 	
-	// Use this for initialization
-	void Start () {
+	[Range(0,1)] float movementFactor; // 1 is full move, 0 is no move
+	Vector3 startingPos;
+	[SerializeField] float period = 0.5f;
+
+    // Use this for initialization
+    void Start () {
 		startingPos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		float cycle = Time.time * period;
+		const float tau = Mathf.PI * 2;
+		float rawSineWave = Mathf.Sin(cycle * tau);
+		movementFactor = rawSineWave / 2f + 0.5f;
+
 		Vector3 offset = movementVector * movementFactor;
 		transform.position = startingPos + offset;
 	}
